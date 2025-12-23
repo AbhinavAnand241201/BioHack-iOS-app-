b1a80304fc4e7912addeda1bc1c8b24429bbcc93
AbhinavAnand241201 <iabhinavanand10@gmail.com>
2025-12-23 20:00:12 +0530

    Initial Commit

---
+++ BioHack/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BioHack
+//
+//  Created by ABHINAV ANAND  on 23/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
